a50e0f9eaff042af3f36218928d2c0f5d44cc956
lab302-6 <lab302-6@labeng.local>
2020-02-25 16:54:25 +0700

    first

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -18,4 +18,34 @@ int main(){
 	return 0;
 }
 
-//Write definition of shuffle() here 
+void shuffle(int &a,int &b, int &c, int &d)
+{
+	int y,z; 
+	y == rand()%100+1;
+	int temp1,temp2,temp3,temp4;
+	for(int i = 0;i<y;i++)
+	{
+		z = rand()%4+1;
+		switch(z)
+		{
+			case 1:
+			temp1 = a;
+			a = b;
+			b = temp1;
+			case 2:
+			temp2 = c;
+			c = d;
+			d = temp2;
+			case 3:
+			temp3 = b;
+			b = c;
+			c = temp3;
+			case 4:
+			temp4 = d;
+			d = a;
+			a =temp4;
+		}
+		
+	}
+	
+}
--- lab14_5.cpp
+++ lab14_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); //Modify input arguments to pointer 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,40 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); //Modify input arguments to pointer 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b, int *c, int *d)
+{
+	int y,z; 
+	y == rand()%100+1;
+	int temp1,temp2,temp3,temp4;
+	for(int v = 0;v<y;v++)
+	{
+		z = rand()%4+1;
+		switch(z)
+		{
+			case 1:
+			temp1 = *a;
+			*a = *b;
+			*b = temp1;
+			case 2:
+			temp2 = *c;
+			*c = *d;
+			*d = temp2;
+			case 3:
+			temp3 = *b;
+			*b = *c;
+			*c = temp3;
+			case 4:
+			temp4 = *d;
+			*d = *a;
+			*a =temp4;
+		}
+		
+	}
+}
